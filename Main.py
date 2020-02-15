68f3e24f8a8c03d7dcb3d603daf611b1531793b9
Tyrone Tong <tyrone.tong800@gmail.com>
2020-02-15 10:39:40 -0800

    'html'

--- Main.py
+++ Main.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
+    return render_template('base.html')
+
+@app.route('/register/', methods = ["POST", "GET"])
+def register():
     return render_template('doc.html')
+
+@app.route('/prescribe/')
+def prescribe():
+    return render_template('prescribe.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
