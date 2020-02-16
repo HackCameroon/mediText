047a605fe6ec85c0221e2e967c3249655cf03e6b
Tyrone Tong <tyrone.tong800@gmail.com>
2020-02-15 22:20:08 -0800

    psp

--- pillSMS.py
+++ pillSMS.py
@@ -19,29 +19,29 @@ def second(patient):
     client = Client("ACc0304e02373de6ce19ae6d5c3e9e2fa2", "dfef0a24cdb05d648c80cf67bf44cbd5")
     client.messages.create(to=patient.phone, from_="+13344588868", body=("Hi {}. This is a second message to remind you to take {}. Please reply if you 'YES' if you have taken the drug or 'MISS' 10 minutes have passed since your first text reminder.").format(patient.firstname +" "+ patient.lastname, patient.drug.name.upper()))
 
-app2 = Flask(__name__)
+# app = Flask(__name__)
 
-@app2.route("/sms", methods=['GET', 'POST'])
-def incoming_sms():
-    patient = current_patient
-    """Send a dynamic reply to an incoming text message"""
-    # Get the message the user sent our Twilio number
-    body = request.values.get('Body', None)
+# @app.route("/sms", methods=['GET', 'POST'])
+# def incoming_sms():
+#     patient = current_patient
+#     """Send a dynamic reply to an incoming text message"""
+#     # Get the message the user sent our Twilio number
+#     body = request.values.get('Body', None)
 
-    # Start our TwiML response
-    resp = MessagingResponse()
+#     # Start our TwiML response
+#     resp = MessagingResponse()
 
-    # Determine the right reply for this message
-    if body.lower() == 'yes':
-        resp.message("Hi!")
-    elif body.lower == 'miss':
-        if (patient.drug.strict_dosage):
-            resp.message("{} has a strict dosage so please try not to miss your next dosage.".format(patient.drug.name.upper()))
-        else:
-            resp.message("Because {} does not have a strict dosage please take the missed dosage and try not to miss your next scheduled dosage.".format(patient.drug.name.upper()))
-    else:
-        resp.message("Invalid response")
-    return str(resp)
+#     # Determine the right reply for this message
+#     if body.lower() == 'yes':
+#         resp.message("Thank you for telling us! We will notify you when your next dose is.")
+#     elif body.lower == 'miss':
+#         if (patient.drug.strict_dosage):
+#             resp.message("{} has a strict dosage so please try not to miss your next dosage.".format(patient.drug.name.upper()))
+#         else:
+#             resp.message("Because {} does not have a strict dosage please take the missed dosage and try not to miss your next scheduled dosage.".format(patient.drug.name.upper()))
+#     else:
+#         resp.message("Invalid response")
+#     return str(resp)
 
 if __name__ == "__main__":
-    app2.run(debug=True)
+    pass
