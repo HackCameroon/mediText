8d19cd2cdb01b7e96c388778d419584e5f7172dc
Tyrone Tong <tyrone.tong800@gmail.com>
2020-02-15 16:19:23 -0800

    draft

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template, request
+from flask import Flask, url_for, render_template, request, redirect
 import classes
 
 app = Flask(__name__)
@@ -29,7 +29,7 @@ def pres():
         bday = request.form['bday']
         if classes.patient_exist(name, bday) != False:
             current_patient = classes.patient_exist(name, bday)
-            return render_template('prescribing.html')
+            return redirect(url_for('prescribing'))
 
     return render_template('prescribe.html')
 
@@ -39,12 +39,18 @@ def prescribing():
         drug = request.form['drug']
         dosage = request.form['dosage']
         comments = request.form['comments']
+        strict = request.form['strict']
         
         new_doctor = classes.Doctor("John Smith","John", "Smith", "5555555555")
         classes.prescribe(new_doctor, current_patient, drug, comments)
-
+        if strict:
+            current_patient.drug.change_strict()
+        return redirect(url_for('success'))
     return render_template('prescribing.html')
 
+@app.route('/success')
+def success():
+    return render_template('success.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
